03a04dd3b87153ebcd4aa566b6db9c0ea82caaa3
Thies Edeling <cac31te@ripe.net>
2011-03-11 10:30:12 +0000

    Removed passing a writer to the xmlserializer to the public API

--- rpki-commons-util/src/main/java/net/ripe/certification/client/xml/XmlSerializer.java
+++ rpki-commons-util/src/main/java/net/ripe/certification/client/xml/XmlSerializer.java
@@ -1,11 +1,8 @@
 package net.ripe.certification.client.xml;
 
-import java.io.Writer;
-
 public interface XmlSerializer<T> {
 
     String serialize(T object);
-    void serialize(T object, Writer writer);
 
     T deserialize(String xml);
 
