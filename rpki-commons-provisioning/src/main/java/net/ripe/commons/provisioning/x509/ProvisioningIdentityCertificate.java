fb9cad4e451444ebbc181d2ca990bceb72c420e9
Thies Edeling <cac31te@ripe.net>
2011-03-21 10:10:58 +0000

    CERT-47 Made PIC serializable so it's easier to store and cache

--- rpki-commons-provisioning/src/main/java/net/ripe/commons/provisioning/x509/ProvisioningIdentityCertificate.java
+++ rpki-commons-provisioning/src/main/java/net/ripe/commons/provisioning/x509/ProvisioningIdentityCertificate.java
@@ -1,8 +1,11 @@
 package net.ripe.commons.provisioning.x509;
 
+import java.io.Serializable;
 import java.security.cert.X509Certificate;
 
-public class ProvisioningIdentityCertificate extends ProvisioningCertificate {
+public class ProvisioningIdentityCertificate extends ProvisioningCertificate implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     public ProvisioningIdentityCertificate(X509Certificate certificate) {
         super(certificate);
