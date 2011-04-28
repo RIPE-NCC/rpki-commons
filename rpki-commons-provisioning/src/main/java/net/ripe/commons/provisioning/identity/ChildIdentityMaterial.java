01f845b89a0122b44a5b408ef07485d6e1d8e954
Thies Edeling <cac31te@ripe.net>
2011-04-28 16:27:29 +0000

    Added serializable

--- rpki-commons-provisioning/src/main/java/net/ripe/commons/provisioning/identity/ChildIdentityMaterial.java
+++ rpki-commons-provisioning/src/main/java/net/ripe/commons/provisioning/identity/ChildIdentityMaterial.java
@@ -2,17 +2,19 @@ package net.ripe.commons.provisioning.identity;
 
 import net.ripe.commons.provisioning.x509.ProvisioningIdentityCertificate;
 
+import java.io.Serializable;
+
 /**
  * Wrapper class for the identity information relevant to <b>MY CHILD</b> in the provisioning protocol
  */
-public class ChildIdentityMaterial {
-    
+public class ChildIdentityMaterial implements Serializable {
+
     private ProvisioningIdentityCertificate childCertificate;
 
     public ChildIdentityMaterial(ProvisioningIdentityCertificate childCertificate) {
         this.childCertificate = childCertificate;
     }
-    
+
     public ProvisioningIdentityCertificate getChildCertificate() {
         return childCertificate;
     }
