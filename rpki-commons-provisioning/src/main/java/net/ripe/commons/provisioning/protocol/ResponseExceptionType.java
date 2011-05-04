c47aca8326c84bae5df14b11a4d87e3b1a357fb2
Thies Edeling <cac31te@ripe.net>
2011-05-04 11:57:57 +0000

    Added description

--- rpki-commons-provisioning/src/main/java/net/ripe/commons/provisioning/protocol/ResponseExceptionType.java
+++ rpki-commons-provisioning/src/main/java/net/ripe/commons/provisioning/protocol/ResponseExceptionType.java
@@ -1,17 +1,23 @@
 package net.ripe.commons.provisioning.protocol;
 
 public enum ResponseExceptionType {
-    BAD_DATA(400 /*HttpServletResponse.SC_BAD_REQUEST*/),
+    BAD_DATA(400 /*HttpServletResponse.SC_BAD_REQUEST*/, "Could not validate client's request"),
 
-    THROTTLING(503 /*HttpServletResponse.SC_SERVICE_UNAVAILABLE*/);
+    THROTTLING(503 /*HttpServletResponse.SC_SERVICE_UNAVAILABLE*/, "The server cannot handle your request at this time");
 
     private int httpResponseCode;
+    private String description;
 
-    private ResponseExceptionType(int httpResponseCode) {
+    private ResponseExceptionType(int httpResponseCode, String description) {
         this.httpResponseCode = httpResponseCode;
+        this.description = description;
     }
 
     public int getHttpResponseCode() {
         return httpResponseCode;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
