6d09fe0f5984db22de3c1a5dea76c9405fa51be3
Erik Rozendaal <cac2er@ripe.net>
2012-04-04 12:15:11 +0000

    Configure rsync timeout. Default is 5 minutes.

--- src/main/java/net/ripe/commons/certification/rsync/Rsync.java
+++ src/main/java/net/ripe/commons/certification/rsync/Rsync.java
@@ -36,50 +36,61 @@ import java.util.List;
 
 public class Rsync {
 
-	private static final String COMMAND = "rsync";
+    public static final int DEFAULT_TIMEOUT_IN_SECONDS = 300;
 
-	private Command command;
+    private static final String COMMAND = "rsync";
 
-	private String source;
-	private String destination;
-	private List<String> options = new ArrayList<String>();
+    private Command command;
 
-	public Rsync() {
-	}
+    private String source;
+    private String destination;
+    private List<String> options = new ArrayList<String>();
+    private int timeoutInSeconds = DEFAULT_TIMEOUT_IN_SECONDS;
 
-	public Rsync(String source, String destination) {
-		this.source = source;
-		this.destination = destination;
-	}
+    public Rsync() {
+    }
 
-	public void addOptions(String... options) {
-		for (int i = 0; i < options.length; i++) {
-			if (options[i] != null) {
-				this.options.add(options[i]);
-			}
-		}
-	}
+    public Rsync(String source, String destination) {
+        this.source = source;
+        this.destination = destination;
+    }
 
-	public void addOptions(Collection<String> options) {
-		for (String option : options) {
-			if (option != null) {
-				this.options.add(option);
-			}
-		}
-	}
+    /**
+     * @param timeoutInSeconds the rsync(1) communication timeout in seconds.
+     */
+    public void setTimeoutInSeconds(int timeoutInSeconds) {
+        if (timeoutInSeconds < 0) throw new IllegalArgumentException("timeout must be non-negative");
+        this.timeoutInSeconds = timeoutInSeconds;
+    }
 
-	public boolean containsOption(String option) {
-	    return options.contains(option);
-	}
+    public void addOptions(String... options) {
+        for (int i = 0; i < options.length; i++) {
+            if (options[i] != null) {
+                this.options.add(options[i]);
+            }
+        }
+    }
 
-	public void reset() {
-		command = null;
-		options.clear();
-		source = null;
-		destination = null;
-	}
+    public void addOptions(Collection<String> options) {
+        for (String option : options) {
+            if (option != null) {
+                this.options.add(option);
+            }
+        }
+    }
 
-	public String getSource() {
+    public boolean containsOption(String option) {
+        return options.contains(option);
+    }
+
+    public void reset() {
+        command = null;
+        options.clear();
+        source = null;
+        destination = null;
+    }
+
+    public String getSource() {
         return source;
     }
 
@@ -96,37 +107,38 @@ public class Rsync {
     }
 
     public String[] getErrorLines() {
-		return command == null ? null : command.getErrorLines();
-	}
+        return command == null ? null : command.getErrorLines();
+    }
 
-	public String[] getOutputLines() {
-		return command == null ? null : command.getOutputLines();
-	}
+    public String[] getOutputLines() {
+        return command == null ? null : command.getOutputLines();
+    }
 
-	public boolean isCompleted() {
-		return command == null ? false : command.isCompleted();
-	}
+    public boolean isCompleted() {
+        return command == null ? false : command.isCompleted();
+    }
 
-	public int getExitStatus() {
-		return command == null ? -1 : command.getExitStatus();
-	}
+    public int getExitStatus() {
+        return command == null ? -1 : command.getExitStatus();
+    }
 
-	public Exception getException() {
-		return command == null ? null : command.getException();
-	}
+    public Exception getException() {
+        return command == null ? null : command.getException();
+    }
 
-	public int execute() {
-		List<String> args = new ArrayList<String>();
-		args.add(COMMAND);
-		args.addAll(options);
-		if ((source != null) && (destination != null)) {
-			args.add(source);
-			args.add(destination);
-		}
+    public int execute() {
+        List<String> args = new ArrayList<String>();
+        args.add(COMMAND);
+        args.add("--timeout=" + timeoutInSeconds);
+        args.addAll(options);
+        if ((source != null) && (destination != null)) {
+            args.add(source);
+            args.add(destination);
+        }
 
-		Command rsync = new Command(args);
-		rsync.execute();
-		command = rsync;
-		return rsync.getExitStatus();
-	}
+        Command rsync = new Command(args);
+        rsync.execute();
+        command = rsync;
+        return rsync.getExitStatus();
+    }
 }
