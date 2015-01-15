6cf6f579ba300e5f9a0cdd3d744941efc4abcded
Oleg Muravskiy <oleg@ripe.net>
2015-01-15 17:54:32 +0100

    Validation messages in case cert has no manifest

--- src/main/java/net/ripe/rpki/commons/validation/ValidationString.java
+++ src/main/java/net/ripe/rpki/commons/validation/ValidationString.java
@@ -181,6 +181,7 @@ public final class ValidationString {
     public static final String VALIDATOR_MANIFEST_DOES_NOT_CONTAIN_FILE = "validator.manifest.does.not.contain.file";
     public static final String VALIDATOR_OBJECT_PROCESSING_EXCEPTION = "validator.object.processing.exception";
     public static final String VALIDATOR_MANIFEST_LOCATION_MISMATCH = "validator.manifest.location.mismatch";
+    public static final String VALIDATOR_CA_SHOULD_HAVE_MANIFEST = "validator.ca.should.have.manifest";
 
     // Problems with repository
     public static final String VALIDATOR_REPOSITORY_INCOMPLETE = "validator.repository.incomplete";
