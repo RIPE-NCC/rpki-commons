e4f37f3b66dc516762c95947f5186da1e375fc78
Oleg Muravskiy <oleg@ripe.net>
2015-02-16 10:29:11 +0100

    Logic to remove expired objects from store

--- src/main/java/net/ripe/rpki/commons/validation/ValidationString.java
+++ src/main/java/net/ripe/rpki/commons/validation/ValidationString.java
@@ -193,5 +193,5 @@ public final class ValidationString {
     public static final String VALIDATOR_REPOSITORY_OBJECT_NOT_IN_CACHE = "validator.repository.object.not.in.cache";
     public static final String VALIDATOR_REPOSITORY_OBJECT_NOT_FOUND = "validator.repository.object.not.found";
     public static final String VALIDATOR_REPOSITORY_UNSUPPORTED_GHOSTBUSTERS_RECORD = "validator.repository.unsupported.ghostbusters.record";
-
+    public static final String VALIDATOR_REPOSITORY_EXPIRED_REVOKED_OBJECT = "validator.repository.expired.revoked.object";
 }
