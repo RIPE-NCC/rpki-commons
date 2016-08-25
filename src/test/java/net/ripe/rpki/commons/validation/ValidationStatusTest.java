9fbbaf59683a168d38a3141b19f137e5c3daa55f
Oleg Muravskiy <oleg@ripe.net>
2016-08-25 15:21:04 +0200

    Fix formatting error message for FETCH_ERRORs.

--- src/test/java/net/ripe/rpki/commons/validation/ValidationStatusTest.java
+++ src/test/java/net/ripe/rpki/commons/validation/ValidationStatusTest.java
@@ -29,10 +29,16 @@
  */
 package net.ripe.rpki.commons.validation;
 
-public enum ValidationStatus {
-    ERROR, WARNING, PASSED, FETCH_ERROR;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-    public String getMessageKey() {
-        return (this == FETCH_ERROR ? ERROR : this).name().toLowerCase();
+public class ValidationStatusTest {
+    @Test
+    public void should_format_message_key() throws Exception {
+        assertEquals("error", ValidationStatus.ERROR.getMessageKey());
+        assertEquals("error", ValidationStatus.FETCH_ERROR.getMessageKey());
+        assertEquals("passed", ValidationStatus.PASSED.getMessageKey());
+        assertEquals("warning", ValidationStatus.WARNING.getMessageKey());
     }
+
 }
