5db6f968f7947ff246d6f1624cd58e029be1e158
Erik Rozendaal <erik@deler.org>
2020-12-24 09:25:43 +0100

    The req_resource_set_* attributes are optional
    See https://tools.ietf.org/html/rfc6492#section-3.4.1.
    
    We use `null` as a "not-present" value. Later we may want to switch to
    use more explicit `Optional` types.

--- src/main/java/net/ripe/rpki/commons/provisioning/payload/issue/request/CertificateIssuanceRequestPayloadSerializer.java
+++ src/main/java/net/ripe/rpki/commons/provisioning/payload/issue/request/CertificateIssuanceRequestPayloadSerializer.java
@@ -66,9 +66,9 @@ public class CertificateIssuanceRequestPayloadSerializer extends AbstractProvisi
         Element element = (Element) requestElements.item(0);
         CertificateIssuanceRequestElement request = new CertificateIssuanceRequestElement();
         request.setClassName(getRequiredAttributeValue(element, "class_name"));
-        request.setAllocatedAsn(IP_RESOURCE_SET_PROVISIONING_CONVERTER.fromString(getRequiredAttributeValue(element, "req_resource_set_as")));
-        request.setAllocatedIpv4(IP_RESOURCE_SET_PROVISIONING_CONVERTER.fromString(getRequiredAttributeValue(element, "req_resource_set_ipv4")));
-        request.setAllocatedIpv6(IP_RESOURCE_SET_PROVISIONING_CONVERTER.fromString(getRequiredAttributeValue(element, "req_resource_set_ipv6")));
+        request.setAllocatedAsn(getAttributeValue(element, "req_resource_set_as").map(IP_RESOURCE_SET_PROVISIONING_CONVERTER::fromString).orElse(null));
+        request.setAllocatedIpv4(getAttributeValue(element, "req_resource_set_ipv4").map(IP_RESOURCE_SET_PROVISIONING_CONVERTER::fromString).orElse(null));
+        request.setAllocatedIpv6(getAttributeValue(element, "req_resource_set_ipv6").map(IP_RESOURCE_SET_PROVISIONING_CONVERTER::fromString).orElse(null));
         request.setCertificateRequest(new PKCS10CertificationRequest(Base64.getMimeDecoder().decode(element.getTextContent())));
         return new CertificateIssuanceRequestPayload(request);
     }
@@ -78,9 +78,15 @@ public class CertificateIssuanceRequestPayloadSerializer extends AbstractProvisi
         CertificateIssuanceRequestElement request = payload.getRequestElement();
         Element node = document.createElementNS(xmlns, "request");
         node.setAttribute("class_name", request.getClassName());
-        node.setAttribute("req_resource_set_as", IP_RESOURCE_SET_PROVISIONING_CONVERTER.toString(request.getAllocatedAsn()));
-        node.setAttribute("req_resource_set_ipv4", IP_RESOURCE_SET_PROVISIONING_CONVERTER.toString(request.getAllocatedIpv4()));
-        node.setAttribute("req_resource_set_ipv6", IP_RESOURCE_SET_PROVISIONING_CONVERTER.toString(request.getAllocatedIpv6()));
+        if (request.getAllocatedAsn() != null) {
+            node.setAttribute("req_resource_set_as", IP_RESOURCE_SET_PROVISIONING_CONVERTER.toString(request.getAllocatedAsn()));
+        }
+        if (request.getAllocatedIpv4() != null) {
+            node.setAttribute("req_resource_set_ipv4", IP_RESOURCE_SET_PROVISIONING_CONVERTER.toString(request.getAllocatedIpv4()));
+        }
+        if (request.getAllocatedIpv6() != null) {
+            node.setAttribute("req_resource_set_ipv6", IP_RESOURCE_SET_PROVISIONING_CONVERTER.toString(request.getAllocatedIpv6()));
+        }
         node.setTextContent(Base64.getEncoder().encodeToString(request.getCertificateRequest().getEncoded()));
         return Collections.singletonList(node);
     }
