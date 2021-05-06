9722e9e64cc132886a87744b6109f4f1d06fb9ed
Mikhail Puzanov <mpuzanov@ripe.net>
2021-05-06 12:30:54 +0200

    Update src/main/java/net/ripe/rpki/commons/crypto/x509cert/X509CertificateBuilderHelper.java
    Co-authored-by: Bart Bakker <bart@thesoftwarecraft.com>

--- src/main/java/net/ripe/rpki/commons/crypto/x509cert/X509CertificateBuilderHelper.java
+++ src/main/java/net/ripe/rpki/commons/crypto/x509cert/X509CertificateBuilderHelper.java
@@ -88,7 +88,7 @@ import java.util.EnumSet;
  *
  * There are complicated relationships between these two ways of setting resources
  * (@see X509CertificateBuilderHelper.validateResource)
- * which results in a inevitable runtime check. Thus moving resource setting
+ * which results in an inevitable runtime check. Thus moving resource setting
  * into a separate resource-specialised builder doesn't make sense.
  *
  */
