462d2771025df9653ba05cb01cfdfc05edad5a7d
Ties de Kock <ties@tiesdekock.nl>
2022-02-24 17:27:33 +0100

    Validate that IdentityCertificate is self-signed

--- src/main/java/net/ripe/rpki/commons/provisioning/x509/ProvisioningCertificate.java
+++ src/main/java/net/ripe/rpki/commons/provisioning/x509/ProvisioningCertificate.java
@@ -4,6 +4,11 @@ import net.ripe.rpki.commons.crypto.x509cert.AbstractX509CertificateWrapper;
 
 import java.security.cert.X509Certificate;
 
+/**
+ * A provisioning certificate.
+ *
+ * This could be a RFC6492 EE certificate, but can also be a (self-signed) identity certificate.
+ */
 public class ProvisioningCertificate extends AbstractX509CertificateWrapper {
 
     private static final long serialVersionUID = 1L;
--- src/main/java/net/ripe/rpki/commons/provisioning/x509/ProvisioningIdentityCertificate.java
+++ src/main/java/net/ripe/rpki/commons/provisioning/x509/ProvisioningIdentityCertificate.java
@@ -1,13 +1,29 @@
 package net.ripe.rpki.commons.provisioning.x509;
 
 import java.io.Serializable;
+import java.security.InvalidKeyException;
+import java.security.SignatureException;
 import java.security.cert.X509Certificate;
 
+/**
+ * A rfc8183 identity certificate.
+ *
+ * This is a self-signed X.509 BPKI certificate that will be the issuer of the BPKI EE certificates that the child uses
+ * when sending provisioning protocol messages to the parent.
+ */
 public class ProvisioningIdentityCertificate extends ProvisioningCertificate implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+
     public ProvisioningIdentityCertificate(X509Certificate certificate) {
         super(certificate);
+
+        // Check that certificate is self-signed
+        try {
+            this.verify(this.getPublicKey());
+        } catch (InvalidKeyException | SignatureException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 }
