3e45f45071d0d62bd9214a3a2fa69d33233c891a
Mikhail Puzanov <misha.puzanov@gmail.com>
2022-05-10 17:05:16 +0200

    Refactor/rename.

--- src/main/java/net/ripe/rpki/commons/util/RepositoryObjectType.java
+++ src/main/java/net/ripe/rpki/commons/util/RepositoryObjectType.java
@@ -2,6 +2,8 @@ package net.ripe.rpki.commons.util;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 public enum RepositoryObjectType {
 
     Manifest("mft"),
@@ -13,34 +15,19 @@ public enum RepositoryObjectType {
     Unknown("unknown");
 
     @Getter
-    private final String typeName;
+    private final String fileExtension;
 
-    RepositoryObjectType(String typeName) {
-        this.typeName = typeName;
+    RepositoryObjectType(String fileExtension) {
+        this.fileExtension = fileExtension;
     }
 
     /**
      * See https://www.iana.org/assignments/rpki/rpki.xhtml for the list of registered RPKI file extensions
      */
     public static RepositoryObjectType parse(String name) {
-        if (name.endsWith(".mft")) {
-            return Manifest;
-        }
-        if (name.endsWith(".crl")) {
-            return Crl;
-        }
-        if (name.endsWith(".cer")) {
-            return Certificate;
-        }
-        if (name.endsWith(".roa")) {
-            return Roa;
-        }
-        if (name.endsWith(".gbr")) {
-            return Gbr;
-        }
-        if (name.endsWith(".asa")) {
-            return Aspa;
-        }
-        return Unknown;
+        return Arrays.stream(RepositoryObjectType.values())
+            .filter(t -> name.endsWith("." + t.getFileExtension()))
+            .findFirst()
+            .orElse(Unknown);
     }
 }
