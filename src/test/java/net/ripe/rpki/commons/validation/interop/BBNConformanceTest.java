5d24d2d6d8f06512e79890325261efe2fa836036
Ties de Kock <ties@tiesdekock.nl>
2024-02-23 14:21:44 +0100

    Update non-bouncy castle dependencies and remove commons-io

--- src/test/java/net/ripe/rpki/commons/validation/interop/BBNConformanceTest.java
+++ src/test/java/net/ripe/rpki/commons/validation/interop/BBNConformanceTest.java
@@ -1,21 +1,19 @@
 package net.ripe.rpki.commons.validation.interop;
 
-import com.google.common.io.Files;
 import net.ripe.rpki.commons.crypto.CertificateRepositoryObject;
-import net.ripe.rpki.commons.crypto.cms.ghostbuster.GhostbustersCmsParser;
-import net.ripe.rpki.commons.crypto.cms.manifest.ManifestCmsParser;
-import net.ripe.rpki.commons.crypto.cms.roa.RoaCmsParser;
-import net.ripe.rpki.commons.crypto.crl.X509Crl;
 import net.ripe.rpki.commons.crypto.util.CertificateRepositoryObjectFactory;
-import net.ripe.rpki.commons.crypto.x509cert.X509ResourceCertificateParser;
 import net.ripe.rpki.commons.validation.ValidationResult;
-import org.apache.commons.io.FileUtils;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class BBNConformanceTest {
@@ -24,35 +22,37 @@ public class BBNConformanceTest {
 
     @Test
     public void shouldParseAllObjects() throws IOException {
-        int objectCount = 0;
-        int errorCount = 0;
-        int exceptionCount = 0;
+        var objectCount = new AtomicInteger();
+        var errorCount = new AtomicInteger();
+        var exceptionCount = new AtomicInteger();
 
-        Iterator<File> fileIterator = FileUtils.iterateFiles(new File(PATH_TO_BBN_OBJECTS), new String[]{"cer", "crl", "mft", "roa"}, true);
-        while (fileIterator.hasNext()) {
-            objectCount++;
-            File file = fileIterator.next();
-            byte[] encoded = Files.toByteArray(file);
-            ValidationResult result = ValidationResult.withLocation(file.getName());
+        var extensionMatcher = FileSystems.getDefault().getPathMatcher("glob:**.{cer,crl,mft,roa}");
 
-            try {
-                final CertificateRepositoryObject res = CertificateRepositoryObjectFactory.createCertificateRepositoryObject(encoded, result);
-                // Check that invariant that "parsing errors or result" holds.
-                assertTrue(result.hasFailures() || res != null);
+        try (Stream<Path> paths = Files.find(new File(PATH_TO_BBN_OBJECTS).toPath(), Integer.MAX_VALUE, (p, attr) -> extensionMatcher.matches(p))) {
+            paths.forEach(path -> {
+                objectCount.incrementAndGet();
+                try {
+                    byte[] encoded = Files.readAllBytes(path);
+                    ValidationResult result = ValidationResult.withLocation(path.getFileName().toString());
 
-                if (result.hasFailures() && file.getName().startsWith("good")) {
-                    System.err.println("Supposed to be good: " + file.getName());
-                    errorCount++;
-                } else if (! result.hasFailures() && file.getName().startsWith("bad")) {
-                    System.err.println("Supposed to be bad: " + file.getName());
-                    errorCount++;
-                } else {
-                    System.out.println(file.getName() + " -> " + result.hasFailures());
+                    final CertificateRepositoryObject res = CertificateRepositoryObjectFactory.createCertificateRepositoryObject(encoded, result);
+                    // Check that invariant that "parsing errors or result" holds.
+                    assertThat(result.hasFailures() || res != null).isTrue();
+
+                    if (result.hasFailures() && path.getFileName().startsWith("good")) {
+                        System.err.println("Supposed to be good: " + path.getFileName());
+                        errorCount.incrementAndGet();
+                    } else if (!result.hasFailures() && path.getFileName().startsWith("bad")) {
+                        System.err.println("Supposed to be bad: " + path.getFileName());
+                        errorCount.incrementAndGet();
+                    } else {
+                        System.out.println(path.getFileName() + " -> " + result.hasFailures());
+                    }
+                } catch (IOException | RuntimeException ex) {
+                    System.err.println("Exception while parsing " + path.getFileName());
+                    exceptionCount.incrementAndGet();
                 }
-            } catch (RuntimeException ex) {
-                System.err.println("Exception while parsing " + file.getName() );
-                exceptionCount++;
-            }
+            });
         }
 
         System.out.println(objectCount + " objects: " + errorCount + " errors, " + exceptionCount + " exceptions");
