1bf86f60de320da91241a2f79f400b45c2391869
Ties de Kock <ties@tiesdekock.nl>
2024-02-27 08:59:20 +0100

    Beta version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 
     `java-library`
     `signing`
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0-rc-2"
 }
 
 repositories {
