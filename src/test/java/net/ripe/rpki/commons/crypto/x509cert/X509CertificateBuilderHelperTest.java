d7e16008529bcf93d284e4df597a6a154e7c3d93
Ties de Kock <ties@tiesdekock.nl>
2024-04-05 11:59:28 +0200

    Fix linter warnings in X509CertificateBuilderHelperTest

--- src/test/java/net/ripe/rpki/commons/crypto/x509cert/X509CertificateBuilderHelperTest.java
+++ src/test/java/net/ripe/rpki/commons/crypto/x509cert/X509CertificateBuilderHelperTest.java
@@ -5,13 +5,13 @@ import net.ripe.ipresource.IpResourceType;
 import net.ripe.rpki.commons.crypto.ValidityPeriod;
 import net.ripe.rpki.commons.util.UTC;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.x509.Extension;
 import org.bouncycastle.asn1.x509.PolicyInformation;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import javax.annotation.meta.TypeQualifier;
 import javax.security.auth.x500.X500Principal;
 import java.math.BigInteger;
 import java.security.cert.CertificateParsingException;
@@ -20,7 +20,8 @@ import java.util.EnumSet;
 
 import static net.ripe.rpki.commons.crypto.util.KeyPairFactoryTest.*;
 import static net.ripe.rpki.commons.crypto.x509cert.AbstractX509CertificateWrapper.POLICY_INFORMATION;
-import static org.junit.Assert.assertNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class X509CertificateBuilderHelperTest {
@@ -28,7 +29,7 @@ public class X509CertificateBuilderHelperTest {
 
     private X509CertificateBuilderHelper subject;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         subject = new X509CertificateBuilderHelper();
 
@@ -41,61 +42,68 @@ public class X509CertificateBuilderHelperTest {
         subject.withResources(IpResourceSet.ALL_PRIVATE_USE_RESOURCES);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void shouldFailOnIncorrectAlgorithm() {
         subject.withSignatureAlgorithm("foo");
-        subject.generateCertificate();
+        assertThatThrownBy(() -> subject.generateCertificate())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void shouldMakeSureTheresNoExtendedKeyUsage() throws CertificateParsingException {
         final X509Certificate x509Certificate = subject.generateCertificate();
-        assertNull(x509Certificate.getExtendedKeyUsage());
+        assertThat(x509Certificate.getExtendedKeyUsage()).isNull();
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void shouldFailOnEmptyResources() {
         subject.withResources(new IpResourceSet());
-        subject.generateCertificate();
+        assertThatThrownBy(() -> subject.generateCertificate())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void shouldNotFailOnOneInheritResourceType() {
         subject.withResources(new IpResourceSet());
         subject.withInheritedResourceTypes(EnumSet.of(IpResourceType.IPv4));
-        subject.generateCertificate();
+        assertThat(subject.generateCertificate()).isNotNull();
     }
 
+    @Test
     public void shouldAcceptArbitraryPolicyWhichShouldBeCritical() {
         // Take a non-RPKI policy
         subject.withPolicies(CAB_BASELINE_REQUIREMENTS_POLICY);
         // And ensure it is critical
-        subject.generateCertificate().getCriticalExtensionOIDs().contains(CAB_BASELINE_REQUIREMENTS_POLICY.getPolicyIdentifier().toString());
+        assertThat(subject.generateCertificate().getCriticalExtensionOIDs()).contains(Extension.certificatePolicies.toString());
     }
 
-    @Test(expected = IllegalStateException.class)
+    @Test
     public void shouldFailOnRepeatedPolicies() {
         // Policy is set once (e.g. in constructor of supertype).
         subject.withPolicies(POLICY_INFORMATION);
         // And further attempts to set it are rejected (e.g. in an subtype).
-        subject.withPolicies(CAB_BASELINE_REQUIREMENTS_POLICY);
+        assertThatThrownBy(() -> subject.withPolicies(CAB_BASELINE_REQUIREMENTS_POLICY))
+                .isInstanceOf(IllegalStateException.class);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void shouldFailOnNegativeSerial() {
         subject.withSerial(BigInteger.ONE.negate());
-        subject.generateCertificate();
+        assertThatThrownBy(() -> subject.generateCertificate())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void shouldFailOnNeutralIntegerSerial() {
         subject.withSerial(BigInteger.ZERO);
-        subject.generateCertificate();
+        assertThatThrownBy(() -> subject.generateCertificate())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void shouldFailOnTooLargeSerial() {
         subject.withSerial(BigInteger.ONE.shiftLeft(160));
-        subject.generateCertificate();
+        assertThatThrownBy(() -> subject.generateCertificate())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
