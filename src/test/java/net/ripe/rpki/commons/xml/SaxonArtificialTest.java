77dbdd9ea802c2bbac0b5001cef92d39738b8ae1
Mikhail Puzanov <misha.puzanov@gmail.com>
2025-04-10 12:33:58 +0200

    Add a test to force Saxon dependency to be present

--- src/test/java/net/ripe/rpki/commons/xml/SaxonArtificialTest.java
+++ src/test/java/net/ripe/rpki/commons/xml/SaxonArtificialTest.java
@@ -1,2 +1,15 @@
-package net.ripe.rpki.commons.xml;public class SaxonArtificialTest {
+package net.ripe.rpki.commons.xml;
+
+import net.sf.saxon.Version;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+// This test exists only to prevent the Saxon dependency from being removed.
+// It is needed at runtime by rpki-core.
+public class SaxonArtificialTest {
+    @Test
+    public void testSaxonDependencyExists() {
+        assertEquals("SAXON", Version.getProductName());
+    }
 }
