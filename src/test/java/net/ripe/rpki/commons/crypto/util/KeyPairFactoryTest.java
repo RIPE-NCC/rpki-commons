4bf368a72b2811284a5a699d4f227a31ba710423
Mikhail Puzanov <misha.puzanov@gmail.com>
2025-09-04 13:25:56 +0200

    Initial changes for better EC key pair support

--- src/test/java/net/ripe/rpki/commons/crypto/util/KeyPairFactoryTest.java
+++ src/test/java/net/ripe/rpki/commons/crypto/util/KeyPairFactoryTest.java
@@ -3,39 +3,26 @@ package net.ripe.rpki.commons.crypto.util;
 import org.junit.Test;
 
 import java.security.KeyPair;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
-import java.util.HashMap;
-import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class KeyPairFactoryTest {
 
-    public static final String DEFAULT_KEYPAIR_GENERATOR_PROVIDER = "SunRsaSign";
-
     public static KeyPair TEST_KEY_PAIR = PregeneratedKeyPairFactory.getInstance().generate();
     public static KeyPair SECOND_TEST_KEY_PAIR = PregeneratedKeyPairFactory.getInstance().generate();
 
-    private static final Map<String, KeyPair> cachedKeyPairs = new HashMap<String, KeyPair>();
-
-
-    public static KeyPair getKeyPair(String name) {
-        synchronized (cachedKeyPairs) {
-            KeyPair result = cachedKeyPairs.get(name);
-            if (result == null) {
-                result = PregeneratedKeyPairFactory.getInstance().generate();
-                cachedKeyPairs.put(name, result);
-            }
-            return result;
-        }
-    }
-
+    public static KeyPair TEST_EC_KEY_PAIR = PregeneratedKeyPairFactory.getInstance().generateEC();
+    public static KeyPair SECOND_EC_TEST_KEY_PAIR = PregeneratedKeyPairFactory.getInstance().generateEC();
 
     @Test
-    public void shouldGenerateRsaKeyPairs() {
-        KeyPair keyPair = new KeyPairFactory(DEFAULT_KEYPAIR_GENERATOR_PROVIDER).generate();
+    public void shouldGenerateRsaKeyPairsByDefault() {
+        KeyPair keyPair = new KeyPairFactory().generate();
         assertTrue(keyPair.getPublic() instanceof RSAPublicKey);
         assertTrue(keyPair.getPrivate() instanceof RSAPrivateKey);
 
@@ -44,12 +31,20 @@ public class KeyPairFactoryTest {
 
         RSAPublicKey rsaPublicKey = (RSAPublicKey) keyPair.getPublic();
         assertEquals("RSA", rsaPublicKey.getAlgorithm());
-        assertEquals(KeyPairFactory.RPKI_KEY_PAIR_SIZE, rsaPublicKey.getModulus().bitLength());
+        assertEquals(KeyPairFactory.RPKI_RSA_KEY_PAIR_SIZE, rsaPublicKey.getModulus().bitLength());
     }
 
-    @Test(expected = RuntimeException.class)
-    public void shouldKeypairGenerationFailOnInvalidProvider() {
-        new KeyPairFactory("invalid_provider").generate();
+    @Test
+    public void shouldGenerateEcKeyPairsWhenAsked() {
+        KeyPair keyPair = new KeyPairFactory().generateEC();
+        assertTrue(keyPair.getPublic() instanceof ECPublicKey);
+        assertTrue(keyPair.getPrivate() instanceof ECPrivateKey);
+
+        assertEquals(keyPair.getPublic(), KeyPairFactory.decodePublicKeyEC(keyPair.getPublic().getEncoded()));
+        assertEquals(keyPair.getPrivate(), KeyPairFactory.decodePrivateKeyEC(keyPair.getPrivate().getEncoded()));
+
+        ECPublicKey ecPublicKey = (ECPublicKey) keyPair.getPublic();
+        assertEquals("EC", ecPublicKey.getAlgorithm());
     }
 
     @Test(expected = RuntimeException.class)
--- src/test/java/net/ripe/rpki/commons/crypto/util/PregeneratedKeyPairFactory.java
+++ src/test/java/net/ripe/rpki/commons/crypto/util/PregeneratedKeyPairFactory.java
@@ -8,36 +8,33 @@ import net.ripe.rpki.commons.util.UTC;
 import org.joda.time.DateTime;
 
 import javax.security.auth.x500.X500Principal;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * Caches generated keys in a key store so that they can be reused in the next test run. FOR TESTING ONLY!
  */
 public final class PregeneratedKeyPairFactory extends KeyPairFactory {
 
-    private static final PregeneratedKeyPairFactory INSTANCE = new PregeneratedKeyPairFactory("SunRsaSign");
+    private static final PregeneratedKeyPairFactory INSTANCE = new PregeneratedKeyPairFactory();
 
     private static final char[] PASSPHRASE = "passphrase".toCharArray();
 
-    private File keyStoreFile = new File(".pregenerated-test-key-pairs.keystore");
+    private final File keyStoreFile = new File(".pregenerated-test-key-pairs.keystore");
     private KeyStore pregeneratedKeys;
 
     private int count = 0;
 
-    private PregeneratedKeyPairFactory(String provider) {
-        super(provider);
+    private PregeneratedKeyPairFactory() {
+        super();
         initKeyStore();
     }
 
@@ -59,12 +56,16 @@ public final class PregeneratedKeyPairFactory extends KeyPairFactory {
     }
 
     @Override
-    public KeyPairFactory withProvider(String provider) {
-        return this;
+    public KeyPair generateEC() {
+        return generateImpl(super::generateEC, PregeneratedKeyPairFactory::createECCertificate);
     }
 
     @Override
-    public synchronized KeyPair generate() {
+    public KeyPair generate() {
+        return generateImpl(super::generate, PregeneratedKeyPairFactory::createCertificate);
+    }
+
+    public synchronized KeyPair generateImpl(Supplier<KeyPair> keyPairSupplier, Function<KeyPair, X509ResourceCertificate> makeCert) {
         try {
             String alias = "key_" + count;
             ++count;
@@ -72,8 +73,9 @@ public final class PregeneratedKeyPairFactory extends KeyPairFactory {
             PrivateKey key = (PrivateKey) pregeneratedKeys.getKey(alias, PASSPHRASE);
             KeyPair result;
             if (key == null) {
-                result = super.generate();
-                pregeneratedKeys.setKeyEntry(alias, result.getPrivate(), PASSPHRASE, new Certificate[]{createCertificate(result).getCertificate()});
+                result = keyPairSupplier.get();
+                var certificate = makeCert.apply(result).getCertificate();
+                pregeneratedKeys.setKeyEntry(alias, result.getPrivate(), PASSPHRASE, new Certificate[]{certificate});
                 try (final OutputStream output = new FileOutputStream(keyStoreFile)) {
                     pregeneratedKeys.store(output, PASSPHRASE);
                 }
@@ -89,7 +91,22 @@ public final class PregeneratedKeyPairFactory extends KeyPairFactory {
 
     private static X509ResourceCertificate createCertificate(KeyPair keyPair) {
         X509ResourceCertificateBuilder builder = new X509ResourceCertificateBuilder();
-        builder.withSignatureProvider("SunRsaSign");
+        builder.withSignatureProvider(DEFAULT_RSA_KEYPAIR_GENERATOR_PROVIDER);
+        builder.withSerial(BigInteger.ONE);
+        final DateTime now = UTC.dateTime();
+        builder.withValidityPeriod(new ValidityPeriod(now.minusYears(2), now.minusYears(1)));
+        builder.withCa(false);
+        builder.withIssuerDN(new X500Principal("CN=issuer"));
+        builder.withSubjectDN(new X500Principal("CN=subject"));
+        builder.withResources(IpResourceSet.parse("AS1-AS10,10/8,ffc0::/16"));
+        builder.withSigningKeyPair(keyPair);
+        builder.withPublicKey(keyPair.getPublic());
+        return builder.build();
+    }
+
+    private static X509ResourceCertificate createECCertificate(KeyPair keyPair) {
+        X509ResourceCertificateBuilder builder = new X509ResourceCertificateBuilder();
+        builder.withSignatureProvider(DEFAULT_EC_KEYPAIR_GENERATOR_PROVIDER);
         builder.withSerial(BigInteger.ONE);
         final DateTime now = UTC.dateTime();
         builder.withValidityPeriod(new ValidityPeriod(now.minusYears(2), now.minusYears(1)));
