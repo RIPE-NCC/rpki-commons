0e2b4cc3c87f9e0230f83c10be379720c5139916
Mikhail Puzanov <misha.puzanov@gmail.com>
2025-09-08 16:06:20 +0200

    Some preliminary version

--- src/main/java/net/ripe/rpki/commons/crypto/util/KeyPairFactory.java
+++ src/main/java/net/ripe/rpki/commons/crypto/util/KeyPairFactory.java
@@ -9,7 +9,8 @@ public class KeyPairFactory {
     public static final String DEFAULT_RSA_KEYPAIR_GENERATOR_PROVIDER = "SunRsaSign";
     public static final String DEFAULT_EC_KEYPAIR_GENERATOR_PROVIDER = "SunEC";
 
-    public static final String ALGORITHM = "RSA";
+    public static final String RSA_ALGORITHM = "RSA";
+    public static final String ALGORITHM = RSA_ALGORITHM;
     private static final BigInteger RSA_PUBLIC_EXPONENT = RSAKeyGenParameterSpec.F4;
 
     static final int RPKI_RSA_KEY_PAIR_SIZE = 2048;
@@ -17,30 +18,42 @@ public class KeyPairFactory {
     public static final String ECDSA_ALGORITHM = "EC";
     public static final String ECDSA_CURVE = "secp256r1";
 
-    private final static KeyPairGenerator rsaGenerator;
-    private final static KeyPairGenerator ecGenerator;
+    private KeyPairGenerator rsaGenerator;
+    private KeyPairGenerator ecGenerator;
 
-    static {
-        try {
-            rsaGenerator = KeyPairGenerator.getInstance(ALGORITHM, DEFAULT_RSA_KEYPAIR_GENERATOR_PROVIDER);
-            rsaGenerator.initialize(new RSAKeyGenParameterSpec(RPKI_RSA_KEY_PAIR_SIZE, RSA_PUBLIC_EXPONENT));
-
-            ecGenerator = KeyPairGenerator.getInstance(ECDSA_ALGORITHM, DEFAULT_EC_KEYPAIR_GENERATOR_PROVIDER);
-            ecGenerator.initialize(new ECGenParameterSpec(ECDSA_CURVE), new SecureRandom());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    synchronized KeyPairGenerator getEcGenerator() {
+        if (ecGenerator == null) {
+            try {
+                ecGenerator = KeyPairGenerator.getInstance(ECDSA_ALGORITHM, DEFAULT_EC_KEYPAIR_GENERATOR_PROVIDER);
+                ecGenerator.initialize(new ECGenParameterSpec(ECDSA_CURVE), new SecureRandom());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
+        return ecGenerator;
+    }
+
+    synchronized KeyPairGenerator getRsaGenerator() {
+        if (rsaGenerator == null) {
+            try {
+                rsaGenerator = KeyPairGenerator.getInstance(RSA_ALGORITHM, DEFAULT_RSA_KEYPAIR_GENERATOR_PROVIDER);
+                rsaGenerator.initialize(new RSAKeyGenParameterSpec(RPKI_RSA_KEY_PAIR_SIZE, RSA_PUBLIC_EXPONENT));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return rsaGenerator;
     }
 
     public KeyPairFactory() {
     }
 
     public KeyPair generate() {
-        return rsaGenerator.generateKeyPair();
+        return getRsaGenerator().generateKeyPair();
     }
 
     public KeyPair generateEC() {
-        return ecGenerator.generateKeyPair();
+        return getEcGenerator().generateKeyPair();
     }
 
     /**
@@ -51,7 +64,7 @@ public class KeyPairFactory {
      */
     public static PublicKey decodePublicKey(byte[] encoded) {
         try {
-            return KeyFactory.getInstance(ALGORITHM).generatePublic(new X509EncodedKeySpec(encoded));
+            return KeyFactory.getInstance(RSA_ALGORITHM).generatePublic(new X509EncodedKeySpec(encoded));
         } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
             throw new KeyPairFactoryException(e);
         }
@@ -66,7 +79,7 @@ public class KeyPairFactory {
      */
     public static PrivateKey decodePrivateKey(byte[] encoded) {
         try {
-            return KeyFactory.getInstance(ALGORITHM).generatePrivate(new PKCS8EncodedKeySpec(encoded));
+            return KeyFactory.getInstance(RSA_ALGORITHM).generatePrivate(new PKCS8EncodedKeySpec(encoded));
         } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
             throw new KeyPairFactoryException(e);
         }
@@ -93,5 +106,9 @@ public class KeyPairFactory {
         } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
             throw new KeyPairFactoryException(e);
         }
+    }
+
+    public KeyPairFactory withProvider(String provider) {
+        return new KeyPairFactory(provider);
     }
 }
--- src/test/java/net/ripe/rpki/commons/crypto/util/PregeneratedKeyPairFactory.java
+++ src/test/java/net/ripe/rpki/commons/crypto/util/PregeneratedKeyPairFactory.java
@@ -4,6 +4,8 @@ import net.ripe.ipresource.IpResourceSet;
 import net.ripe.rpki.commons.crypto.ValidityPeriod;
 import net.ripe.rpki.commons.crypto.x509cert.X509ResourceCertificate;
 import net.ripe.rpki.commons.crypto.x509cert.X509ResourceCertificateBuilder;
+import net.ripe.rpki.commons.crypto.x509cert.X509RouterCertificate;
+import net.ripe.rpki.commons.crypto.x509cert.X509RouterCertificateBuilder;
 import net.ripe.rpki.commons.util.UTC;
 import org.joda.time.DateTime;
 
@@ -16,71 +18,73 @@ import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
  * Caches generated keys in a key store so that they can be reused in the next test run. FOR TESTING ONLY!
  */
-public final class PregeneratedKeyPairFactory extends KeyPairFactory {
+public class PregeneratedKeyPairFactory extends KeyPairFactory {
 
-    private static final PregeneratedKeyPairFactory INSTANCE = new PregeneratedKeyPairFactory();
+    private static final PregeneratedKeyPairFactory RSA_INSTANCE = new PregeneratedKeyPairFactory(
+            DEFAULT_RSA_KEYPAIR_GENERATOR_PROVIDER, () -> getRsaGenerator().generateKeyPair());
+
+    private static final PregeneratedKeyPairFactory EC_INSTANCE = new PregeneratedKeyPairFactory(
+            DEFAULT_EC_KEYPAIR_GENERATOR_PROVIDER, () -> getEcGenerator().generateKeyPair());
 
     private static final char[] PASSPHRASE = "passphrase".toCharArray();
+    private final Supplier<KeyPair> kpGenerator;
 
-    private final File keyStoreFile = new File(".pregenerated-test-key-pairs.keystore");
-    private KeyStore pregeneratedKeys;
-
+    private File keyStoreFile;
+    private KeyStore preGeneratedKeys;
     private int count = 0;
+    private final String provider;
 
-    private PregeneratedKeyPairFactory() {
+    private PregeneratedKeyPairFactory(String provider, Supplier<KeyPair> kpGenerator) {
         super();
-        initKeyStore();
+        this.provider = provider;
+        this.kpGenerator = kpGenerator;
+        initKeyStore(provider);
     }
 
-    private void initKeyStore() {
+    private void initKeyStore(String provider) {
         try {
-            pregeneratedKeys = KeyStore.getInstance("JKS", "SUN");
+            keyStoreFile = new File(".pregenerated-test-key-pairs." + provider + ".keystore");
+            preGeneratedKeys = KeyStore.getInstance("JKS", "SUN");
             try (InputStream input = new FileInputStream(keyStoreFile)) {
-                pregeneratedKeys.load(input, PASSPHRASE);
+                preGeneratedKeys.load(input, PASSPHRASE);
             } catch (FileNotFoundException e) {
-                pregeneratedKeys.load(null, PASSPHRASE);
+                preGeneratedKeys.load(null, PASSPHRASE);
             }
         } catch (final IOException | GeneralSecurityException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static PregeneratedKeyPairFactory getInstance() {
-        return INSTANCE;
+    public static PregeneratedKeyPairFactory getRsaInstance() {
+        return RSA_INSTANCE;
+    }
+
+    public static PregeneratedKeyPairFactory getEcInstance() {
+        return EC_INSTANCE;
     }
 
     @Override
-    public KeyPair generateEC() {
-        return generateImpl(super::generateEC, PregeneratedKeyPairFactory::createECCertificate);
-    }
-
-    @Override
-    public KeyPair generate() {
-        return generateImpl(super::generate, PregeneratedKeyPairFactory::createCertificate);
-    }
-
-    public synchronized KeyPair generateImpl(Supplier<KeyPair> keyPairSupplier, Function<KeyPair, X509ResourceCertificate> makeCert) {
+    public synchronized KeyPair generate() {
         try {
             String alias = "key_" + count;
             ++count;
 
-            PrivateKey key = (PrivateKey) pregeneratedKeys.getKey(alias, PASSPHRASE);
+            PrivateKey key = (PrivateKey) preGeneratedKeys.getKey(alias, PASSPHRASE);
             KeyPair result;
             if (key == null) {
-                result = keyPairSupplier.get();
-                var certificate = makeCert.apply(result).getCertificate();
-                pregeneratedKeys.setKeyEntry(alias, result.getPrivate(), PASSPHRASE, new Certificate[]{certificate});
+                result = kpGenerator.get();
+                var certificate = createCertificate(result, provider);
+                preGeneratedKeys.setKeyEntry(alias, result.getPrivate(), PASSPHRASE, new Certificate[]{certificate});
                 try (final OutputStream output = new FileOutputStream(keyStoreFile)) {
-                    pregeneratedKeys.store(output, PASSPHRASE);
+                    preGeneratedKeys.store(output, PASSPHRASE);
                 }
             } else {
-                Certificate certificate = pregeneratedKeys.getCertificateChain(alias)[0];
+                Certificate certificate = preGeneratedKeys.getCertificateChain(alias)[0];
                 result = new KeyPair(certificate.getPublicKey(), key);
             }
             return result;
@@ -89,9 +93,19 @@ public final class PregeneratedKeyPairFactory extends KeyPairFactory {
         }
     }
 
-    private static X509ResourceCertificate createCertificate(KeyPair keyPair) {
+    private static X509Certificate createCertificate(KeyPair keyPair, String provider) {
+        if (provider.equals(DEFAULT_RSA_KEYPAIR_GENERATOR_PROVIDER)) {
+            return createRsaCertificate(keyPair, provider).getCertificate();
+        } else if (provider.equals(DEFAULT_EC_KEYPAIR_GENERATOR_PROVIDER)) {
+            return createEcCertificate(keyPair, provider).getCertificate();
+        } else {
+            throw new IllegalArgumentException("Unknown provider " + provider);
+        }
+    }
+
+    private static X509ResourceCertificate createRsaCertificate(KeyPair keyPair, String provider) {
         X509ResourceCertificateBuilder builder = new X509ResourceCertificateBuilder();
-        builder.withSignatureProvider(DEFAULT_RSA_KEYPAIR_GENERATOR_PROVIDER);
+        builder.withSignatureProvider(provider);
         builder.withSerial(BigInteger.ONE);
         final DateTime now = UTC.dateTime();
         builder.withValidityPeriod(new ValidityPeriod(now.minusYears(2), now.minusYears(1)));
@@ -104,18 +118,18 @@ public final class PregeneratedKeyPairFactory extends KeyPairFactory {
         return builder.build();
     }
 
-    private static X509ResourceCertificate createECCertificate(KeyPair keyPair) {
-        X509ResourceCertificateBuilder builder = new X509ResourceCertificateBuilder();
-        builder.withSignatureProvider(DEFAULT_EC_KEYPAIR_GENERATOR_PROVIDER);
+    private static X509RouterCertificate createEcCertificate(KeyPair keyPair, String provider) {
+        X509RouterCertificateBuilder builder = new X509RouterCertificateBuilder();
         builder.withSerial(BigInteger.ONE);
         final DateTime now = UTC.dateTime();
         builder.withValidityPeriod(new ValidityPeriod(now.minusYears(2), now.minusYears(1)));
         builder.withCa(false);
         builder.withIssuerDN(new X500Principal("CN=issuer"));
         builder.withSubjectDN(new X500Principal("CN=subject"));
-        builder.withResources(IpResourceSet.parse("AS1-AS10,10/8,ffc0::/16"));
         builder.withSigningKeyPair(keyPair);
+        builder.withAsns(new int[]{1, 2, 3});
         builder.withPublicKey(keyPair.getPublic());
         return builder.build();
     }
+
 }
