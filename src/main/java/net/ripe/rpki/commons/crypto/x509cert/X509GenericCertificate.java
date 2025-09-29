c6ad6963373cacf846843da8a287a174c81cc2a0
Mikhail Puzanov <misha.puzanov@gmail.com>
2025-09-29 13:16:06 +0200

    Restructure certificate class hierarchy

--- src/main/java/net/ripe/rpki/commons/crypto/x509cert/X509GenericCertificate.java
+++
@@ -1,9 +0,0 @@
-package net.ripe.rpki.commons.crypto.x509cert;
-
-import java.security.cert.X509Certificate;
-
-public abstract class X509GenericCertificate extends AbstractX509CertificateWrapper implements X509CertificateObject {
-    protected X509GenericCertificate(X509Certificate certificate) {
-        super(certificate);
-    }
-}
