b3525d5df073295f92e891213751b11293bf79c6
Mikhail Puzanov <misha.puzanov@gmail.com>
2025-10-09 10:51:13 +0200

    Cleanup

--- src/test/java/net/ripe/rpki/commons/crypto/util/KeyPairFactoryTest.java
+++ src/test/java/net/ripe/rpki/commons/crypto/util/KeyPairFactoryTest.java
@@ -7,10 +7,9 @@ import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
-import java.util.HashMap;
-import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class KeyPairFactoryTest {
@@ -19,24 +18,10 @@ public class KeyPairFactoryTest {
     public static KeyPair SECOND_TEST_KEY_PAIR = KeyPairFactory.rsa().generate();
 
     public static KeyPair TEST_EC_KEY_PAIR = KeyPairFactory.bgpSec().generate();
-    public static KeyPair SECOND_EC_TEST_KEY_PAIR = KeyPairFactory.bgpSec().generate();
-
-    private static final Map<String, KeyPair> cachedKeyPairs = new HashMap<>();
-
-    public static KeyPair getKeyPair(String name) {
-        synchronized (cachedKeyPairs) {
-            KeyPair result = cachedKeyPairs.get(name);
-            if (result == null) {
-                result = KeyPairFactory.rsa().generate();
-                cachedKeyPairs.put(name, result);
-            }
-            return result;
-        }
-    }
 
     @Test
     public void shouldGenerateRsaKeyPairsByDefault() {
-        KeyPair keyPair = new KeyPairFactory(KeyPairFactory.DEFAULT_RSA_KEYPAIR_GENERATOR_PROVIDER).generate();
+        KeyPair keyPair = KeyPairFactory.rsa().generate();
         assertTrue(keyPair.getPublic() instanceof RSAPublicKey);
         assertTrue(keyPair.getPrivate() instanceof RSAPrivateKey);
 
